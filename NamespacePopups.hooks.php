8ed36f860892c3cac4e1223d4ba6d5ede809868d
Umherirrender <umherirrender_de.wp@web.de>
2017-01-12 20:17:04 +0100

    Fix build
    Similar to I215fabe129933a04644e05eb541cbf3b34699095
    
    Update .gitignore to ignore package manager material (node_modules,
    vendor and composer.lock).
    
    Set manifest_version to 2 and use schema for "requires"
    
    PHP_CodeSniffer:
    
    Fix up white spaces after comment character.
    Add spaces next to parentheses.
    
    PagePopups.hooks.php
    Reindent with tabulations
    
    npm:
    
    Bump grunt to 1.x which now ship with a grunt CLI utility. Hence remove
    grunt-cli which is no more needed.
    
    Remove jscsc task. It does not pass yet, see BoilerPlates extension to
    add it later.
    
    Comment out 'banana' since there is no i18n files yet.
    
    Change-Id: I25fdd0d99dfab483cc7bedd8c7b4e26dcc136978

--- NamespacePopups.hooks.php
+++ NamespacePopups.hooks.php
@@ -3,69 +3,97 @@
 // use MediaWiki\Title;
 
 class NamespacePopupsHooks {
-	public static function onHtmlPageLinkRendererEnd( $renderer, $target, $isKnown, &$text, &$attribs, &$ret ) {
-                global $wgNamespacePopupsNamespaceMap, $wgNamespacePopupsAnchor;
+	public static function onHtmlPageLinkRendererEnd( $renderer, $target, $isKnown, &$text,
+		&$attribs, &$ret
+	) {
+		global $wgNamespacePopupsNamespaceMap, $wgNamespacePopupsAnchor;
 
-                if(!$wgNamespacePopupsNamespaceMap) return true;
+		if ( !$wgNamespacePopupsNamespaceMap ) {
+			return true;
+		}
 
-                // It does not work with $target instanceof TitleValue (as in "search for this page title")
-//                 $linkNS = $target->getSubjectNsText();
+		// It does not work with $target instanceof TitleValue (as in "search for this page title")
+		// $linkNS = $target->getSubjectNsText();
 		global $wgContLang;
 		$linkNS = $wgContLang->getNsText( MWNamespace::getSubject( $target->getNamespace() ) );
 
-                if(!$linkNS) $linkNS = ''; // needed?
+		if ( !$linkNS ) {
+			$linkNS = ''; // needed?
+		}
 
-                $popupNS = isset($wgNamespacePopupsNamespaceMap[$linkNS]) ? $wgNamespacePopupsNamespaceMap[$linkNS] : null;
-                if(!$popupNS) $popupNS = isset($wgNamespacePopupsNamespaceMap['*']) ? $wgNamespacePopupsNamespaceMap['*'] : null;
-                if(!$popupNS) return true;
-                if($popupNS === '*') $popupNS = $linkNS;
+		$popupNS = isset( $wgNamespacePopupsNamespaceMap[$linkNS] )
+			? $wgNamespacePopupsNamespaceMap[$linkNS] : null;
+		if ( !$popupNS ) {
+			$popupNS = isset( $wgNamespacePopupsNamespaceMap['*'] )
+				? $wgNamespacePopupsNamespaceMap['*'] : null;
+		}
+
+		if ( !$popupNS ) {
+			return true;
+		}
+
+		if ( $popupNS === '*' ) {
+			$popupNS = $linkNS;
+		}
 
 		$remains = $target->getDBkey();
-                $anchor = $wgNamespacePopupsAnchor ? $wgNamespacePopupsAnchor : '&uarr;';
-                $page = $popupNS === '' ? $remains : "$popupNS:$remains";
+		$anchor = $wgNamespacePopupsAnchor ? $wgNamespacePopupsAnchor : '&uarr;';
+		$page = $popupNS === '' ? $remains : "$popupNS:$remains";
 		$title = Title::newFromText( $page );
-		if(!$title) return true;
-                $url = $title->getLocalUrl();
+
+		if ( !$title ) {
+			return true;
+		}
+
+		$url = $title->getLocalUrl();
 
 		$html = HtmlArmor::getHtml( $text );
 		$html = Html::rawElement( 'a', $attribs, $html );
+
 		if ( $title->isKnown() ) {
-                        $html .= Html::rawElement( 'a', [ 'class' => 'mw-pagepopup', 'href' => $url ], $anchor );
+			$html .= Html::rawElement( 'a', [ 'class' => 'mw-pagepopup', 'href' => $url ], $anchor );
 		} else {
-                        $html .= Html::rawElement( 'a', [ 'class' => 'mw-pagepopup new', 'href' => $url ], $anchor );
+			$html .= Html::rawElement( 'a', [ 'class' => 'mw-pagepopup new', 'href' => $url ], $anchor );
 		}
 
 		$ret = $html;
-                return false;
+
+		return false;
 	}
 
 	public static function onParserAfterTidy( &$parser, &$text ) {
-                global $wgNamespacePopupsNamespaceMap;
+		global $wgNamespacePopupsNamespaceMap;
 
-                if(!$wgNamespacePopupsNamespaceMap) return;
+		if ( !$wgNamespacePopupsNamespaceMap ) {
+			return;
+		}
 
-                $parserOutput = $parser->getOutput();
+		$parserOutput = $parser->getOutput();
 
-                $oldLinks = [];
-                // The below algorithm enumerates all links. This may be a little inefficient
-                foreach($parserOutput->getLinks() as $linkNSid => $linksArray) {
-                        $linkNS = MWNamespace::getCanonicalName( $linkNSid );
-                        foreach(array_keys($linksArray) as $remains)
-                                $oldLinks[] = [ $linkNS, $remains ];
-                }
+		$oldLinks = [];
+		// The below algorithm enumerates all links. This may be a little inefficient
+		foreach ( $parserOutput->getLinks() as $linkNSid => $linksArray ) {
+			$linkNS = MWNamespace::getCanonicalName( $linkNSid );
+			foreach ( array_keys( $linksArray ) as $remains ) {
+				$oldLinks[] = [ $linkNS, $remains ];
+			}
+		}
 
-                foreach($oldLinks as $linkInfo) {
-                        list($linkNS, $remains) = $linkInfo;
+		foreach ( $oldLinks as $linkInfo ) {
+			list( $linkNS, $remains ) = $linkInfo;
 
-                        $popupNS = @$wgNamespacePopupsNamespaceMap[$linkNS] ?: @$wgNamespacePopupsNamespaceMap['*'];
-                        if(!$popupNS) continue;
-                        if($popupNS === '*') {
-                                $popupPage = $linkPage;
-                        } else {
-                                $popupPage = $popupNS === '' ? $remains : "$popupNS:$remains";
-                        }
+			$popupNS = isset( $wgNamespacePopupsNamespaceMap[$linkNS] )
+				? $wgNamespacePopupsNamespaceMap[$linkNS] : $wgNamespacePopupsNamespaceMap['*'];
+			if ( !$popupNS ) {
+				continue;
+			}
+			if ( $popupNS === '*' ) {
+				$popupPage = $linkPage;
+			} else {
+				$popupPage = $popupNS === '' ? $remains : "$popupNS:$remains";
+			}
 
-                        $parserOutput->addLink( Title::newFromDBkey($popupPage) );
-                }
+			$parserOutput->addLink( Title::newFromDBkey( $popupPage ) );
+		}
 	}
 }
--- NamespacePopups.php
+++ NamespacePopups.php
@@ -3,10 +3,10 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'NamespacePopups' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	//$wgMessagesDirs['NamespacePopups'] = __DIR__ . '/i18n';
+	// $wgMessagesDirs['NamespacePopups'] = __DIR__ . '/i18n';
 	/* wfWarn(
-		'Deprecated PHP entry point used for NamespacePopups extension. Please use wfLoadExtension instead, ' .
-		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+		'Deprecated PHP entry point used for NamespacePopups extension. Please use wfLoadExtension ' .
+		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	); */
 	return;
 } else {
