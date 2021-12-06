7f07aa93ee7aa6bbf87404151126f4bff58ed3f4
Umherirrender <umherirrender_de.wp@web.de>
2021-12-06 17:16:41 +0100

    Emit deprecation warning from php entry point
    Change-Id: I4c3a7871713468c243f3857fb423b76e93a8edae

--- NamespacePopups.php
+++ NamespacePopups.php
@@ -3,12 +3,12 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'NamespacePopups' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	// $wgMessagesDirs['NamespacePopups'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	$wgMessagesDirs['NamespacePopups'] = __DIR__ . '/i18n';
+	wfWarn(
 		'Deprecated PHP entry point used for NamespacePopups extension. Please use wfLoadExtension ' .
 		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the NamespacePopups extension requires MediaWiki 1.32+' );
+	die( 'This version of the NamespacePopups extension requires MediaWiki 1.34+' );
 }
